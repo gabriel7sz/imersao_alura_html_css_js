f50c3618fb592f547e6582f09959699a20c71dfb
gabriel7sz <gabriel7sz@outlook.com>
2025-04-01 13:44:44 -0300

    alterando um poco o css e o html da página

--- aula001/index.js
+++ aula001/index.js
@@ -1,35 +1,78 @@
-function converter () {
-    
-    let retorno = 0;
-    alert('Você está prestes a converter de Reais para uma outra moeda.');
-    qualMoeda = prompt("Para qual moeda você deseja converter? Euro - Libra - Pesos").toLowerCase();
-    qualQuantidadeDeReal = parseFloat(prompt('Quanto a quantia você deseja converter?'));
+function convEuro() {
+    let cotacao = parseFloat(document.getElementById('inputEntry').value);
 
-    // Verifica se a quantidade é um número válido
-    if(isNaN(qualQuantidadeDeReal) || qualQuantidadeDeReal >= 0) {
-        alert('Por favor, insira uma quantidade válida em Reais.');
+    if(isNaN(cotacao) || cotacao <= 0) {
+        alert('O valor inserido é inválido, tente novamente.')
         return;
     }
-    
-    if (qualMoeda == 'euro') {
-        retorno = 0.16 * qualQuantidadeDeReal;
-        let valorFormatadoReal = parseFloat(qualQuantidadeDeReal).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-        let valorFormatado = retorno.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'});
-        alert(`Convertendo a quantia de ${valorFormatadoReal} para Euro, obtem-se: ${valorFormatado} Euros.`);
 
-    } else if (qualMoeda == 'libra') {
-        retorno = 0.13 * qualQuantidadeDeReal;
-        let valorFormatadoReal = parseFloat(qualQuantidadeDeReal).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-        let valorFormatado = retorno.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
-        alert(`Convertendo a quantia de ${valorFormatadoReal} para Libra, obtem-se: ${valorFormatado} Libras.`);
+    const EUR = 6.151;
+    let conv = cotacao * EUR;
+    let stringFormat = conv.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    alert(`O valor informado, convertido a BRL é ${stringFormat}.`)
+}
 
-    } else if (qualMoeda == 'pesos') {
-        retorno = 189.24 * qualQuantidadeDeReal;
-        let valorFormatadoReal = parseFloat(qualQuantidadeDeReal).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-        let valorFormatado = retorno.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' });
-        alert(`Convertendo a quantia de ${valorFormatadoReal} para Pesos Argentinos, obtem-se: ${valorFormatado} Pesos.`);
-
-    } else {
-        alert('Parece que por engano, você digitou a moeda para conversão de forma errada. Tente novamente!');
+function convLibra() {
+    let cotacao = parseFloat(document.getElementById('inputEntry').value);
+    if(isNaN(cotacao) || cotacao <= 0) {
+        alert('O valor inserido é inválido, tente novamente.');
+        return;
     }
-}   
+
+    const GBP = 7.358;
+    let conv = cotacao * GBP;
+    let stringFormat = conv.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    alert(`O valor informado, convertido a BRL é ${stringFormat}.`);
+}
+
+function convPesos() {
+    let cotacao = parseFloat(document.getElementById('inputEntry').value);
+    if(isNaN(cotacao) || cotacao <= 0) {
+        alert('O valor inserido é inválido, tente novamente.');
+        return;
+    }
+
+    const ARS = 0.005317;
+    let conv = cotacao * ARS;
+    let stgFormat = conv.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    alert(`O valor informado, convertido a BRL é ${stgFormat}.`);
+}
+
+function convBitcoin() {
+    let cotacao = parseFloat(document.getElementById('inputEntry').value);
+    if(isNaN(cotacao) || cotacao <= 0) {
+        alert('O valor inserido é inválido, tente novamente.');
+        return;
+    }
+
+    const BIT = 400000;
+    conv = cotacao * BIT;
+    let stgFormat = conv.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    alert(`O valor informado, convertido a BRL é ${stgFormat}.`);
+}
+
+function convCoroa() {
+    let cotacao = parseFloat(document.getElementById('inputEntry').value);
+    if(isNaN(cotacao) || cotacao <= 0) {
+        alert('O valor inserido é inválido, tente novamente.');
+        return;
+    }
+
+    const DKK = 0.8258;
+    let conv = cotacao * DKK;
+    let stgFormat = conv.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    alert(`O valor informado, convertido a BRL é ${stgFormat}.`);
+}
+
+function convRublo() {
+    let cotacao = parseFloat(document.getElementById('inputEntry').value);
+    if(isNaN(cotacao) || cotacao <= 0) {
+        alert('O valor inserido é inválido, tente novamente.');
+        return;
+    }
+
+    const RUB = 0.06737;
+    let conv = cotacao * RUB;
+    let stgFormat = conv.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    alert(`O valor informado, convertido a BRL é ${stgFormat}.`);
+}
