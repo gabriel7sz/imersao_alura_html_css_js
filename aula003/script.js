f87654506b384cec2a41fe74c4fbc336875887f1
gabriel7sz <gabriel7sz@outlook.com>
2025-04-07 15:04:26 -0300

    manutenção no código, tirando coisas desnecessárias, aplicando mais função as functions no js, aprendendo mais sobre css

--- aula003/script.js
+++ aula003/script.js
@@ -1,15 +1,17 @@
 function iDontNow() {
     const removeDataDialog = document.getElementById('dialog').innerHTML = '';
+    const changeHeight = document.getElementById('content').classList.add('maxHeight');
     const addData = document.getElementById('dialog').innerHTML = "<p>Se você nunca ouviu Pink Floyd, saiba que Pink Floyd foi uma banda britânica de rock formada em Londres em 1965, que se tornou um dos grupos mais influentes e comercialmente bem-sucedidos da história da música.<p>"
 }
 
-
 function iHeardAbout() {
     const removeDataDialog = document.getElementById('dialog').innerHTML = '';
+    const changeHeight = document.getElementById('content').classList.add('maxHeight');
     const addData = document.getElementById('dialog').innerHTML = '<p>Como você já ouviu falar, você deve saber que a banda Pink Floyd, possui características marcantes como:<br />Música psicodélica e progressiva; Shows elaborados;<br />Letras filosóficas e Capas de álbuns icônicas;</p>';
 }
 
 function iReallyLike() {
     const removeDataDialog = document.getElementById('dialog').innerHTML = '';
+    const changeHeight = document.getElementById('content').classList.add('maxHeight');
     const addData = document.getElementById('dialog').innerHTML = '<p>Eu tamém gosto muito! Pink Floyd é uma banda que transcende gerações, com sua música atemporal e sua influência duradoura na cultura pop.<br /> Aqui alguns de seus principais álbuns:<br />The Dark Side of the Moon; Wish You Were Here;<br /> The Piper at  the Gates of Dawn; Animals; The Wall.</p>';
 }
